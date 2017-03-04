fddc58e20b9ff176e96b69176892c567f27910d0
Marcin Majewski <majewskimarcin@outlook.com>
2017-03-04 12:44:15 +0100

    * Initialization * Creationg of some utility classes

---
+++ lab1/src/main/java/nlp/lab1/ngram/statistics/FileReader.java
@@ -0,0 +1,26 @@
+package nlp.lab1.ngram.statistics;
+
+import com.google.common.base.Preconditions;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class FileReader {
+
+	private static final Logger logger = Logger.getLogger(FileReader.class.getName());
+
+	public static String getContentOfFile(File sourceFile){
+		Preconditions.checkArgument(sourceFile.exists(), "File does not exist!");
+		String wholeFile = null;
+		try {
+			wholeFile = FileUtils.readFileToString(sourceFile, "UTF-8");
+		} catch (IOException e) {
+			logger.warning("Exception during reading from file to string !");
+		}
+		return wholeFile;
+	}
+
+
+}
