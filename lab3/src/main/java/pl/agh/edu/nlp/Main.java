90bd33b57251cc2178a66fc48cff04ed8ca6c5d4
Marcin Majewski <majewskimarcin@outlook.com>
2017-03-19 14:14:12 +0100

    * Finishing task * Adding output

--- lab3/src/main/java/pl/agh/edu/nlp/Main.java
+++ lab3/src/main/java/pl/agh/edu/nlp/Main.java
@@ -20,12 +20,13 @@ public class Main {
 		String input = classLoader.getResource("potop.txt").getFile();
 		StatisticsCollector statisticsCollector = new StatisticsCollector(new File(input), dictionary);
 		final String outputPath = "src\\main\\resources\\output\\";
-		statisticsCollector.createFileWithRanking(new File(outputPath + "ranking.txt"));
-		statisticsCollector.getCumlativePercentageDataForZipfLaw(new File(outputPath + "zipfLawPercentage.txt"));
+		statisticsCollector.createFileWithRanking(new File("ranking.txt"));
+		statisticsCollector.getCumulativePercentageDataForZipfLaw(new File("zipfLawPercentage.txt"));
 		logger.info("Hapax legonema: {}", statisticsCollector.getHapaxLegonema());
 		logger.info("Number of words that are 50% counting from the top of ranked list: {}",
 				statisticsCollector.get50PercentageFromTop());
 		logger.info("Number of words that are 50% counting from the botton of ranked list: {}",
 				statisticsCollector.get50PercentageFromBottom());
+		statisticsCollector.getOutputRankingListNGrams(new File("outputDiGrams.txt"), new File("outputTriGrams.txt"));
 	}
 }
--- lab3/src/main/java/pl/agh/edu/nlp/services/StatisticsCollector.java
+++ lab3/src/main/java/pl/agh/edu/nlp/services/StatisticsCollector.java
@@ -22,6 +22,8 @@ import static java.util.Collections.sort;
 public class StatisticsCollector {
 	private static final Logger logger = LoggerFactory.getLogger(StatisticsCollector.class);
 	private Map<String, Integer> wordToOccurrence = new HashMap<>();
+	private Map<String, Integer> diGramsOccurrence = new HashMap<>();
+	private Map<String, Integer> triGramsOccurrence = new HashMap<>();
 	private List<WordToOccurrence> rankedList = new ArrayList<>();
 	private Dictionary defaultFormDictionary;
 	private int totalNumberOfWords = 0;
@@ -30,7 +32,84 @@ public class StatisticsCollector {
 		this.defaultFormDictionary = defaultFormDictionary;
 		initializeWordOccurrence(inputFile);
 		createSortedListOfOccurrence();
+		createDiGramsAndTriGramsStatistics(inputFile);
 		logger.info("Initializing of StatisticsCollector finished");
+	}
+
+	private void createDiGramsAndTriGramsStatistics(File inputFile) {
+		String contentOfFile = FileReader.getContentOfFile(inputFile);
+		List<String> allWords = getWordsFromString(contentOfFile);
+		for (String word : allWords) {
+			List<String> diGrams = getNGrams(2, word);
+			List<String> triGrams = getNGrams(3, word);
+
+			//fixme can refactor
+			for (String diGram : diGrams) {
+				if (diGramsOccurrence.containsKey(diGram)) {
+					diGramsOccurrence.put(diGram, diGramsOccurrence.get(diGram) + 1);
+				} else {
+					diGramsOccurrence.put(diGram, 1);
+				}
+			}
+
+			for (String triGram : triGrams) {
+				if (triGramsOccurrence.containsKey(triGram)) {
+					triGramsOccurrence.put(triGram, triGramsOccurrence.get(triGram) + 1);
+				} else {
+					triGramsOccurrence.put(triGram, 1);
+				}
+			}
+		}
+
+		logger.info("Finishing diGrams and triGrams statistics");
+	}
+
+	public void getOutputRankingListNGrams(File outputFileDiGrams, File outputFileTrigrams) {
+		List<WordToOccurrence> diGramsRankingList;
+		List<WordToOccurrence> triGramsRankingList;
+
+		diGramsRankingList = createRankingList(diGramsOccurrence);
+		triGramsRankingList = createRankingList(triGramsOccurrence);
+
+		StringBuilder stringBuilder = new StringBuilder();
+		for (WordToOccurrence wordToOccurrence : diGramsRankingList) {
+			stringBuilder.append(wordToOccurrence.getWord()).append("\t").append(wordToOccurrence.getOccurrence());
+			stringBuilder.append(System.lineSeparator());
+		}
+		String diGramsRanking = stringBuilder.toString();
+
+		stringBuilder = new StringBuilder();
+		for (WordToOccurrence wordToOccurrence : triGramsRankingList) {
+			stringBuilder.append(wordToOccurrence.getWord()).append("\t").append(wordToOccurrence.getOccurrence());
+			stringBuilder.append(System.lineSeparator());
+		}
+		String triGramsRanking = stringBuilder.toString();
+
+		try {
+			FileUtils.writeStringToFile(outputFileDiGrams, diGramsRanking);
+			FileUtils.writeStringToFile(outputFileTrigrams, triGramsRanking);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private List<WordToOccurrence> createRankingList(Map<String, Integer> occurrenceMap) {
+		return occurrenceMap.entrySet()
+				.stream()
+				.map(entry -> new WordToOccurrence(entry.getKey(), entry.getValue()))
+				.sorted()
+				.collect(Collectors.toList());
+	}
+
+	private List<String> getNGrams(int n, String word) {
+		int length = word.length();
+		List<String> nGrams = new ArrayList<>();
+		for (int i = 0; i + n < length + 1; i++) {
+			String ngram = word.substring(i, i + n);
+			nGrams.add(ngram);
+		}
+		return nGrams;
 	}
 
 	public void createFileWithRanking(File file) {
@@ -52,8 +131,10 @@ public class StatisticsCollector {
 			stringBuilder.append(System.lineSeparator());
 		}
 
+		String content = stringBuilder.toString();
+
 		try {
-			FileUtils.writeStringToFile(file, stringBuilder.toString());
+			FileUtils.writeStringToFile(file, content);
 		} catch (IOException e) {
 			logger.error("Failed while trying to write string to : {}", file.getAbsolutePath());
 		}
@@ -90,13 +171,19 @@ public class StatisticsCollector {
 				.collect(Collectors.toList());
 	}
 
-	public void getCumlativePercentageDataForZipfLaw(File outputFile) {
+	public void getCumulativePercentageDataForZipfLaw(File outputFile) {
 		double sum = 0d;
 		if (outputFile.exists()) {
-			outputFile.delete();
+			boolean result = outputFile.delete();
+			if (!result) {
+				logger.error("Cannot delete file: {}", outputFile.getAbsolutePath());
+			}
 		}
 		try {
-			outputFile.createNewFile();
+			boolean result = outputFile.createNewFile();
+			if (!result) {
+				logger.error("Cannot create file: {}", outputFile.getAbsolutePath());
+			}
 		} catch (IOException e) {
 			logger.error("Cannot create new file : {}", outputFile.getAbsolutePath());
 		}
@@ -117,18 +204,6 @@ public class StatisticsCollector {
 			logger.error("Cannot write string to file: {}", outputFile.getAbsolutePath());
 		}
 		logger.info("Sum of percentage: {}", sum * 100);
-	}
-
-	public void printOutMandelbrotGraph() {
-
-	}
-
-	public void countHapaxLegomena() {
-
-	}
-
-	public void printOutOccurenceOfWords() {
-
 	}
 
 	public Integer getHapaxLegonema() {
