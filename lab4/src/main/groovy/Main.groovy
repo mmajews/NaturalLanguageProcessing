40afd7f29a2ade359240c4d6afa4dcdf3d178fa0
Marcin Majewski <mmajewski@guidewire.com>
2017-03-29 17:03:36 +0200

    * Initial configuration for lab 5

--- lab4/src/main/groovy/Main.groovy
+++ lab4/src/main/groovy/Main.groovy
@@ -9,7 +9,7 @@ class Main {
     static void main(String[] args) {
         logger.info("Starting app...")
         StatisticsCollector statisticsCollector = new StatisticsCollector();
-        final resourcesPath = "C:\\Development\\Repos\\NaturalLanguageProcessing\\lab4\\src\\main\\resources"
+        final resourcesPath = "/Users/mmajewski/Development/Studies/NaturalLanguageProcessing/lab4/src/main/resources"
         resourcesPath.normalize()
         statisticsCollector.appendFileToDictionary(new File((resourcesPath + "/dramat.txt").normalize()))
         statisticsCollector.appendFileToDictionary(new File((resourcesPath + "/formy.txt").normalize()))
@@ -18,6 +18,6 @@ class Main {
         statisticsCollector.appendFileToDictionary(new File((resourcesPath + "/publ.txt").normalize()))
         statisticsCollector.appendFileToDictionary(new File((resourcesPath + "/wp.txt").normalize()))
 
-        statisticsCollector.getFileWithFixedWords(new File((resourcesPath + "/bledy.txt").normalize()))
+        statisticsCollector.getFileWithFixedWords(new File((resourcesPath + "/bledy1.txt").normalize()))
     }
 }
