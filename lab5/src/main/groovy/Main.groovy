1e511762d6394f5b3d4ebbed40be97373fe70804
Marcin Majewski <majewskimarcin@outlook.com>
2017-04-10 21:54:37 +0200

    * Fixing main

--- lab5/src/main/groovy/Main.groovy
+++ lab5/src/main/groovy/Main.groovy
@@ -1,4 +1,3 @@
-import database.LanguageModelToDatabaseReader
 import database.MongoService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -7,12 +6,15 @@ import services.ProbabilityFinder
 
 class Main {
     private static Logger logger = LoggerFactory.getLogger(Main.class)
-    private static final String PATH_TO_LANGUAGE_MODEL = "C:\\Development\\Repos\\NaturalLanguageProcessing\\lab5\\polish_3gm_knigi_kn.lm"
+    private static
+    final String PATH_TO_LANGUAGE_MODEL = "C:\\Development\\Repos\\NaturalLanguageProcessing\\lab5\\polish_3gm_knigi_kn.lm"
 
     static void main(String[] args) {
         logger.info("Starting app...")
 //        def languageModel = new LanguageModelToDatabaseReader(PATH_TO_LANGUAGE_MODEL)
         ProbabilityFinder probabilityFinder = new MongoService()
-        print ProbabilityCounter.getNGramModelProbability("dwa zbliżone wyniki z", "2", probabilityFinder)
+        def input = "dwa zbliżone wyniki z"
+        def prob = ProbabilityCounter.getNGramModelProbability(input, "2", probabilityFinder)
+        logger.info("Probability of sentence: \"$input\" is equal to $prob")
     }
 }
