65a954d0dda9cf1ced7833dfc70ed4e7b97ac945
Marcin Majewski <mmajewski@guidewire.com>
2017-04-23 21:42:30 +0200

    * Generating stop list * Slicing text into documents

---
+++ lab6/src/main/groovy/FileReader.groovy
@@ -0,0 +1,56 @@
+import com.google.common.base.Preconditions
+
+import java.util.stream.Collectors
+
+class FileReader {
+
+    static Set<String> generateStopList(List<List<String>> docs, double stopListThreshold) {
+        def stopList = new HashSet<String>()
+        def occurrence = new HashMap<String, Integer>()
+        def numberOfTotalOccurrences = 0d
+
+        for (List<String> document : docs) {
+            for (String el : document) {
+                if (occurrence.containsKey(el)) {
+                    occurrence.put(el, occurrence.get(el) + 1)
+                } else {
+                    occurrence.put(el, 1)
+                }
+                numberOfTotalOccurrences++
+            }
+        }
+
+        for (Map.Entry<String, Integer> ent : occurrence) {
+            if ((ent.getValue() as double) / (numberOfTotalOccurrences as double) > stopListThreshold) {
+                stopList.add(ent.getKey())
+            }
+        }
+        stopList
+    }
+
+
+    static List<List<String>> readFileIntoDocuments(String path) {
+        def inputFile = new File(path)
+        def filePath = inputFile.getAbsolutePath()
+        Preconditions.checkArgument(inputFile.exists(), "File $filePath does not exist")
+        def content = inputFile.text
+        def documents = content.split("#\\d+\\n").toList()
+        documents = documents.stream().filter {
+            el -> el != ""
+        }.collect(Collectors.toList())
+
+
+        def slicedElements = new ArrayList<List<String>>()
+        documents.forEach {
+            el ->
+                def ready = el.split("\\s+").toList().stream().map {
+                    var -> var.toUpperCase().trim().replaceAll("\\p{P}", "")
+                }.collect(Collectors.toList())
+                slicedElements.add(ready)
+        }
+
+        slicedElements
+    }
+
+
+}
--- lab6/src/main/groovy/Main.groovy
+++ lab6/src/main/groovy/Main.groovy
@@ -3,9 +3,13 @@ import org.slf4j.LoggerFactory
 
 class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class)
+    private static final THRESHOLD = 0.005d
 
     static void main(String[] args) {
         logger.info("Starting app...")
+        def sliced = FileReader.readFileIntoDocuments("src/main/resources/pap.txt")
+        def stopList = FileReader.generateStopList(sliced, THRESHOLD)
+        logger.info("Finishing reading file from documents")
     }
 
 }
