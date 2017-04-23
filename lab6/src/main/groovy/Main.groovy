dd3e57a84d0e1c588afd84c3233e8ea757065e26
Marcin Majewski <mmajewski@guidewire.com>
2017-04-23 19:18:26 +0200

    * Initial setup v2

---
+++ lab6/src/main/groovy/Main.groovy
@@ -0,0 +1,11 @@
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class)
+
+    static void main(String[] args) {
+        logger.info("Starting app...")
+    }
+
+}
