98dc592fae191ec689dd41669bbe5199b61dcd18
Marcin Majewski <majewskimarcin@outlook.com>
2017-04-24 23:52:25 +0200

    * Adding terms for searching phrase

--- lab6/src/main/groovy/graph/GraphHelper.groovy
+++ lab6/src/main/groovy/graph/GraphHelper.groovy
@@ -3,11 +3,15 @@ package graph
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+import java.util.stream.Collectors
+
 class GraphHelper {
     static final Logger logger = LoggerFactory.getLogger(GraphHelper.class)
 
     static test(List<List<String>> elements, Set<String> stopList, String toFindSimilar, int k) {
         Set<String> terms = new HashSet<>()
+        terms.addAll(getTermsForString(toFindSimilar))
+
         elements.forEach{
             list -> list.forEach{
                 element ->
@@ -21,6 +25,12 @@ class GraphHelper {
         logger.info("Vertexes created")
         graph.createConnections(elements)
         logger.info("Finished creating connections!")
+    }
+
+    static getTermsForString(String el) {
+        el.split("\\s+").toList().stream().map {
+            var -> var.toUpperCase().trim().replaceAll("\\p{P}", "")
+        }.collect(Collectors.toList())
     }
 
 }
