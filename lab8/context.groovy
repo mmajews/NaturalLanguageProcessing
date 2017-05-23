d370c9d2a73c86af22a8aaf9a596482c8aa19161
Marcin Majewski <majewskimarcin@outlook.com>
2017-05-23 18:40:58 +0200

    * Context search using groovy executing grep

---
+++ lab8/context.groovy
@@ -0,0 +1,9 @@
+@Grab(group = 'com.google.guava', module = 'guava', version = '18.0')
+import com.google.common.base.Preconditions
+
+Preconditions.checkArgument(args.size() == 2, "Put two arguments! 1. lines range 2. word to search for")
+def lines = args[0]
+def toSearchFor = args[1]
+def process = "grep -n -A $lines -B $lines '$toSearchFor' in/potop.txt".execute()
+def output = process.in.text
+println(output)
